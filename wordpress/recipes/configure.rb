ef8c8d8f84f54f4087ffa2a8f72127bb7171fe31
Valerii Bereza <valerii.bereza@sethq.com>
2015-06-11 13:16:18 +0300

    fixes

--- wordpress/recipes/configure.rb
+++ wordpress/recipes/configure.rb
@@ -16,32 +16,36 @@ response = http.request(request)
 keys = response.body
 
 
-# Create the Wordpress config file wp-config.php with corresponding values
 node[:deploy].each do |app_name, deploy|
 
-    template "#{deploy[:deploy_to]}/current/wp-config.php" do
-        source "wp-config.php.erb"
-        mode 0660
-        group deploy[:group]
+  template "#{deploy[:deploy_to]}/current/wp-config.php" do
+    source "wp-config.php.erb"
+    mode 0660
+    group deploy[:group]
 
-        if platform?("ubuntu")
-          owner "www-data"
-        elsif platform?("amazon")
-          owner "apache"
-        end
-
-        variables(
-            :keys => (keys rescue nil),
-            :host => (deploy[:database][:host] rescue nil),
-            :user => (deploy[:database][:username] rescue nil),
-            :password => (deploy[:database][:password] rescue nil),
-            :database => (deploy[:database][:database] rescue nil)
-        )
-        only_if do
-            File.directory?("#{deploy[:deploy_to]}/current")
-        end
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")   
+      owner "apache"
     end
+
+    variables(
+      :host =>     (deploy[:database][:host] rescue nil),
+      :user =>     (deploy[:database][:username] rescue nil),
+      :password => (deploy[:database][:password] rescue nil),
+      :db =>       (deploy[:database][:database] rescue nil),
+      :keys =>     (keys rescue nil),
+      :table =>    (node[:phpapp][:dbtable] rescue nil)
+    )
+
+   only_if do
+     File.directory?("#{deploy[:deploy_to]}/current")
+   end
+  end
 end
+
+
+
 
 # Create a Cronjob for Wordpress
 cron "wordpress" do
